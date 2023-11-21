324f1dc943115aab1498af13f14a43a71a7a7b5a
Alec Tackitt <tackitt_am@outlook.com>
2023-11-21 16:19:41 -0500

    made simple AWP and 1 ball

--- src/autons.cpp
+++ src/autons.cpp
@@ -13,31 +13,23 @@ void Close() {
 
 	//actual code
 	Cata.move_relative(100,100);
-	arms::chassis::move({44,0});
-	arms::chassis::turn(255);
-	wings.set_value(true);
-	intake.move(-100);
-	arms::chassis::move(26);
-	arms::chassis::move(-12, arms::REVERSE);
-	intake.move(0);
-	arms::chassis::turn(140,75);
-	wings.set_value(false);
-	arms::chassis::move(45.25);
-	arms::chassis::turn(-332,75);
+	arms::chassis::turn(60,75);
 	arms::chassis::move(10);
-	arms::chassis::turn(-340);
 	wings.set_value(true);
 	pros::delay(250);
 	arms::chassis::move(-15, arms::REVERSE);
-	arms::chassis::turn(-311);
 	wings.set_value(false);
+	arms::chassis::turn(160);
 	pros::delay(250);
-	arms::chassis::move(-5, arms::REVERSE);
-	arms::chassis::turn(250);
-	arms::chassis::move(13);
-	intake.move(-100);
 	arms::chassis::turn(270);
-	arms::chassis::move(6);
+	arms::chassis::move(5);
+	intake.move(-100);
+	arms::chassis::turn(300);
+	arms::chassis::move(20);
+	arms::chassis::turn(340);
+
+	
+
 	
 
 	
@@ -55,11 +47,11 @@ void far() {
 	pros::lcd::print(2, "far side auto");
 
 	//actual code
-	Cata.move_relative(500,100);
-	intake.move(-100);
-	arms::chassis::turn(30);
-	arms::chassis::move(45);
-	intake.move(100);
+	
+	arms::chassis::turn(315);
+	arms::chassis::move(-35, arms::REVERSE);
+	arms::chassis::move(10);
+	
 
 
 	
